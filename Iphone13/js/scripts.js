22d4ec5c7659703ad2439518664c72e749087a3f
Gleisson Santos <jsgleisson@gmail.com>
2022-11-28 22:42:19 -0300

    commit
    Finalização do projeto

--- Iphone13/js/scripts.js
+++ Iphone13/js/scripts.js
@@ -1,1 +1,29 @@
-console.log("teste")
+
+//Selecionar os elementos a serem trabalhados
+
+const buttons = document.querySelectorAll("#image-picker li");
+const image = document.querySelector("#product-image");
+
+//Coloar um evento para cada um dos botoes com o for
+buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      
+        console.log(e);
+
+        buttons.forEach((btn) => {
+            btn.querySelector(".color").classList.remove("selected");
+        });
+
+        const button = e.target;
+        const id = button.getAttribute("id")
+
+        button.querySelector(".color").classList.add("selected");
+
+        image.classList.add("changing");
+        image.setAttribute("src", `img/iphone_${id}.jpg`);
+        setTimeout (() => {
+            image.classList.toggle("changing");
+        }, 200);
+
+    });
+});
